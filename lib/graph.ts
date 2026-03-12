c84ee9873ab29a929c1ad8b897f1f7f4bdc55a40
bhumidhardoley <bhumidhar414@gmail.com>
2026-03-13 00:57:54 +0530

    Testing

--- lib/graph.ts
+++ lib/graph.ts
@@ -1,36 +1,38 @@
-import { StateSchema, MessagesValue, GraphNode, StateGraph, START, END, interrupt } from "@langchain/langgraph";
+import { StateSchema, MessagesValue, GraphNode, StateGraph, START, END } from "@langchain/langgraph";
 import { ChatOllama } from "@langchain/ollama";
 
+/* Ollama model */
 const model = new ChatOllama({
   model: "qwen2.5",
 });
 
+/* Graph state */
 const State = new StateSchema({
   messages: MessagesValue,
 });
 
-const analystAgent: GraphNode<typeof State> = async (state) => {
+/* Singer Agent */
+const singerAgent: GraphNode<typeof State> = async (state) => {
 
   const response = await model.invoke([
     {
       role: "system",
-      content: "Generate promotional content from a raw organizer prompt, create a series of hype-building posts, analyze past engagement to find optimal posting times, and schedule the content for publishing."
+      content: "You are a singer. Reply to everything like you are singing a song."
     },
     ...state.messages
   ]);
 
-  /* pause execution */
-  interrupt({
-    message: response.content
-  });
-
   return {
-    messages: [response]
+    messages: [response],
   };
 };
 
+/* Angry Agent */
+
+/* Build graph */
 export const graph = new StateGraph(State)
-  .addNode("analyst", analystAgent)
-  .addEdge(START, "analyst")
-  .addEdge("analyst", END)
+  .addNode("singer", singerAgent)
+  .addEdge(START, "singer")
+  .addEdge("singer", END)
+
   .compile();
